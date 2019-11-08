673f5debe1c87c2d93552dd552691deff39072c1
mailtheni <57097790+mailtheni@users.noreply.github.com>
2019-11-08 13:18:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello LearnAzure007!");
+    response.end("Helloo LearnAzure007!");
 });
 
 const port = process.env.PORT || 1337;
